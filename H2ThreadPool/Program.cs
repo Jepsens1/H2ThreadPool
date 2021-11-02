faa5d8bfcb7457558868df2dbcfa168903b1197d
Phillip Svanholt Kjær Jepsen <jepsens123@outlook.dk>
2021-11-02 08:28:59 +0100

    Add project files.

---
+++ H2ThreadPool/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace H2ThreadPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch mywatch = new Stopwatch();
+            Console.WriteLine("Thread Pool Execution");
+            mywatch.Start();
+            ProcessWithThreadPoolMethod();
+            mywatch.Stop();
+            Console.WriteLine("Time consumed by ProcessWithThreadPoolMethod is : " + mywatch.ElapsedTicks.ToString());
+            mywatch.Reset();
+            Console.WriteLine("Thread Execution");
+            mywatch.Start();
+            ProcessWithThreadMethod();
+            mywatch.Stop();
+            Console.WriteLine("Time consumed by ProcessWithThreadMethod is : " + mywatch.ElapsedTicks.ToString());
+        }
+        static void Process(object callback)
+        {
+        }
+        static void ProcessWithThreadMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread t = new Thread(Process);
+                t.Start();
+            }
+        }
+        static void ProcessWithThreadPoolMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Process);
+            }
+        }
+    }
+}
